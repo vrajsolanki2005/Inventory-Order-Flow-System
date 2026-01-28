af4ae3ce642b99c64f4a081004ec3b98d16ef111
vrajsolanki2005 <144586038+vrajsolanki2005@users.noreply.github.com>
2026-01-29 00:10:52 +0530

    login.auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const User = require('../models/userModel');
 const db = require('../config/db');
 const bcrypt = require('bcryptjs');
+const {isStrongPassword} = require('../utils/validator');
 
+
+//register
 router.post('/register', async(req, res) =>{
     const {username, email, password} = req.body;
 
@@ -14,8 +17,8 @@ router.post('/register', async(req, res) =>{
         return res.status(400).json({message: 'Please fill in all fields'});
     }
     //check strong password
-    if(!isStrongPasssword(password)){
-        return res.status(400).json({message: 'Password is not strong ePassword must be at least 8 characters and include uppercase, lowercase, number, and special character'});
+    if(!isStrongPassword(password)){
+        return res.status(400).json({message: 'Password is not strong. Password must be at least 8 characters and include uppercase, lowercase, number, and special character'});
     }
     try{
         //check if already exists
@@ -38,4 +41,35 @@ router.post('/register', async(req, res) =>{
         console.error(error);
         res.status(500).json({message: 'Error creating user'});
     }
-})
+});
+
+//login
+router.post('/login', async(req, res) =>{
+    const {email, password} = req.body;
+
+//validate user
+    if(!email || !password){
+        return res.status(400).json({message: 'Please fill in all fields'});
+    }
+    try{
+        //check email
+        const user = await User.findUserByEmail(email);
+        if(!user){
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+        //check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+        //create token
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+        res.json({token, user:{id: user.id, username: user.username, email: user.email}});
+        
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Error logging in'});
+    }
+});
+
+module.exports = router;
